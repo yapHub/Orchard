7f92535ac46e609bffc2911a40496c28bcc557bf
yapHub <yapHub@users.noreply.github.com>
2017-08-23 10:06:00 +0300

    Fix bug on updating role permission
    Fix bug which was adding role permission combination multiple times when UpdateRole method was called more than once before committing db transaction

--- src/Orchard.Web/Modules/Orchard.Roles/Services/RoleService.cs
+++ src/Orchard.Web/Modules/Orchard.Roles/Services/RoleService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Orchard.Caching;
@@ -9,8 +9,10 @@ using Orchard.Roles.Events;
 using Orchard.Roles.Models;
 using Orchard.Security.Permissions;
 
-namespace Orchard.Roles.Services {
-    public class RoleService : IRoleService {
+namespace Orchard.Roles.Services
+{
+    public class RoleService : IRoleService
+    {
         private const string SignalName = "Orchard.Roles.Services.RoleService";
 
         private readonly IRepository<RoleRecord> _roleRepository;
@@ -27,8 +29,9 @@ namespace Orchard.Roles.Services {
             IRepository<UserRolesPartRecord> userRolesRepository,
             IEnumerable<IPermissionProvider> permissionProviders,
             ICacheManager cacheManager,
-            ISignals signals, 
-            IRoleEventHandler roleEventHandlers) {
+            ISignals signals,
+            IRoleEventHandler roleEventHandlers)
+        {
 
             _roleRepository = roleRepository;
             _permissionRepository = permissionRepository;
@@ -44,32 +47,39 @@ namespace Orchard.Roles.Services {
         public Localizer T { get; set; }
         public ILogger Logger { get; set; }
 
-        public IEnumerable<RoleRecord> GetRoles() {
+        public IEnumerable<RoleRecord> GetRoles()
+        {
             var roles = from role in _roleRepository.Table select role;
             return roles.ToList();
         }
 
-        public RoleRecord GetRole(int id) {
+        public RoleRecord GetRole(int id)
+        {
             return _roleRepository.Get(id);
         }
 
-        public RoleRecord GetRoleByName(string name) {
+        public RoleRecord GetRoleByName(string name)
+        {
             return _roleRepository.Get(x => x.Name == name);
         }
 
-        public void CreateRole(string roleName) {
+        public void CreateRole(string roleName)
+        {
             if (GetRoleByName(roleName) != null)
                 return;
 
-            var roleRecord = new RoleRecord {Name = roleName};
+            var roleRecord = new RoleRecord { Name = roleName };
             _roleRepository.Create(roleRecord);
             _roleEventHandlers.Created(new RoleCreatedContext { Role = roleRecord });
             TriggerSignal();
         }
 
-        public void CreatePermissionForRole(string roleName, string permissionName) {
-            if (_permissionRepository.Get(x => x.Name == permissionName) == null) {
-                _permissionRepository.Create(new PermissionRecord {
+        public void CreatePermissionForRole(string roleName, string permissionName)
+        {
+            if (_permissionRepository.Get(x => x.Name == permissionName) == null)
+            {
+                _permissionRepository.Create(new PermissionRecord
+                {
                     Description = GetPermissionDescription(permissionName),
                     Name = permissionName,
                     FeatureName = GetFeatureName(permissionName)
@@ -80,49 +90,76 @@ namespace Orchard.Roles.Services {
             roleRecord.RolesPermissions.Add(new RolesPermissionsRecord { Permission = permissionRecord, Role = roleRecord });
             _roleEventHandlers.PermissionAdded(new PermissionAddedContext { Role = roleRecord, Permission = permissionRecord });
             TriggerSignal();
-            
+
         }
 
-        public void UpdateRole(int id, string roleName, IEnumerable<string> rolePermissions) {
+        public void UpdateRole(int id, string roleName, IEnumerable<string> rolePermissions)
+        {
             var roleRecord = GetRole(id);
             var currentRoleName = roleRecord.Name;
-            var currentPermissions = roleRecord.RolesPermissions.ToDictionary(x => x.Permission.Name);
-            roleRecord.Name = roleName;
-            roleRecord.RolesPermissions.Clear();
-
-            if (!String.Equals(currentRoleName, roleName)) {
-                _roleEventHandlers.Renamed(new RoleRenamedContext {Role = roleRecord, NewRoleName = roleName, PreviousRoleName = currentRoleName});
+            var groupedPermissions = roleRecord.RolesPermissions.GroupBy(x => x.Permission.Name);
+            var currentPermissions = groupedPermissions.Select(x => x.FirstOrDefault()).ToDictionary(x => x.Permission.Name);
+            
+            // Log error if there are duplicate role permission combination
+            if(groupedPermissions.Any(x => x.Count() > 1))
+            {
+                Logger.Error("Duplicate permissions were found!");
             }
 
-            foreach (var rolePermission in rolePermissions) {
+            roleRecord.Name = roleName;
+
+            if (!String.Equals(currentRoleName, roleName))
+            {
+                _roleEventHandlers.Renamed(new RoleRenamedContext { Role = roleRecord, NewRoleName = roleName, PreviousRoleName = currentRoleName });
+            }
+
+            foreach (var rolePermission in rolePermissions)
+            {
                 string permission = rolePermission;
-                if (_permissionRepository.Get(x => x.Name == permission) == null) {
-                    _permissionRepository.Create(new PermissionRecord {
+                if (_permissionRepository.Get(x => x.Name == permission) == null)
+                {
+                    _permissionRepository.Create(new PermissionRecord
+                    {
                         Description = GetPermissionDescription(permission),
                         Name = permission,
                         FeatureName = GetFeatureName(permission)
                     });
                 }
                 PermissionRecord permissionRecord = _permissionRepository.Get(x => x.Name == permission);
-                roleRecord.RolesPermissions.Add(new RolesPermissionsRecord { Permission = permissionRecord, Role = roleRecord });
+                if (!roleRecord.RolesPermissions.Any(x => x.Permission == permissionRecord && x.Role == roleRecord))
+                {
+                    roleRecord.RolesPermissions.Add(new RolesPermissionsRecord { Permission = permissionRecord, Role = roleRecord });
+                }
 
-                if(!currentPermissions.ContainsKey(permission))
+
+                if (!currentPermissions.ContainsKey(permission))
                     _roleEventHandlers.PermissionAdded(new PermissionAddedContext { Role = roleRecord, Permission = permissionRecord });
-                else {
+                else
+                {
                     currentPermissions.Remove(permission);
                 }
             }
 
-            foreach(var permission in currentPermissions.Values)
+            foreach (var permission in currentPermissions.Values)
+            {
                 _roleEventHandlers.PermissionRemoved(new PermissionRemovedContext { Role = roleRecord, Permission = permission.Permission });
+
+                var permissionToRemove = roleRecord.RolesPermissions
+                           .Where(x => x.Permission == permission.Permission && x.Role == roleRecord).FirstOrDefault();
+                roleRecord.RolesPermissions.Remove(permissionToRemove);
+            }
 
             TriggerSignal();
         }
 
-        private string GetFeatureName(string permissionName) {
-            foreach (var permissionProvider in _permissionProviders) {
-                foreach (var permission in permissionProvider.GetPermissions()) {
-                    if (String.Equals(permissionName, permission.Name, StringComparison.OrdinalIgnoreCase)) {
+        private string GetFeatureName(string permissionName)
+        {
+            foreach (var permissionProvider in _permissionProviders)
+            {
+                foreach (var permission in permissionProvider.GetPermissions())
+                {
+                    if (String.Equals(permissionName, permission.Name, StringComparison.OrdinalIgnoreCase))
+                    {
                         return permissionProvider.Feature.Descriptor.Id;
                     }
                 }
@@ -130,10 +167,14 @@ namespace Orchard.Roles.Services {
             throw new ArgumentException(T("Permission {0} was not found in any of the installed modules.", permissionName).ToString());
         }
 
-        private string GetPermissionDescription(string permissionName) {
-            foreach (var permissionProvider in _permissionProviders) {
-                foreach (var permission in permissionProvider.GetPermissions()) {
-                    if (String.Equals(permissionName, permission.Name, StringComparison.OrdinalIgnoreCase)) {
+        private string GetPermissionDescription(string permissionName)
+        {
+            foreach (var permissionProvider in _permissionProviders)
+            {
+                foreach (var permission in permissionProvider.GetPermissions())
+                {
+                    if (String.Equals(permissionName, permission.Name, StringComparison.OrdinalIgnoreCase))
+                    {
                         return permission.Description;
                     }
                 }
@@ -141,25 +182,30 @@ namespace Orchard.Roles.Services {
             throw new ArgumentException(T("Permission {0} was not found in any of the installed modules.", permissionName).ToString());
         }
 
-        public void DeleteRole(int id) {
+        public void DeleteRole(int id)
+        {
 
             var currentUserRoleRecords = _userRolesRepository.Fetch(x => x.Role.Id == id);
-            foreach(var userRoleRecord in currentUserRoleRecords) {
+            foreach (var userRoleRecord in currentUserRoleRecords)
+            {
                 _userRolesRepository.Delete(userRoleRecord);
             }
 
             var roleRecord = GetRole(id);
             _roleRepository.Delete(roleRecord);
-            _roleEventHandlers.Removed(new RoleRemovedContext { Role = roleRecord});
+            _roleEventHandlers.Removed(new RoleRemovedContext { Role = roleRecord });
             TriggerSignal();
         }
 
-        public IDictionary<string, IEnumerable<Permission>> GetInstalledPermissions() {
+        public IDictionary<string, IEnumerable<Permission>> GetInstalledPermissions()
+        {
             var installedPermissions = new Dictionary<string, IEnumerable<Permission>>();
-            foreach (var permissionProvider in _permissionProviders) {
+            foreach (var permissionProvider in _permissionProviders)
+            {
                 var featureName = permissionProvider.Feature.Descriptor.Id;
                 var permissions = permissionProvider.GetPermissions();
-                foreach (var permission in permissions) {
+                foreach (var permission in permissions)
+                {
                     var category = permission.Category;
 
                     string title = String.IsNullOrWhiteSpace(category) ? T("{0} Feature", featureName).Text : T(category).Text;
@@ -174,17 +220,21 @@ namespace Orchard.Roles.Services {
             return installedPermissions;
         }
 
-        public IEnumerable<string> GetPermissionsForRole(int id) {
+        public IEnumerable<string> GetPermissionsForRole(int id)
+        {
             var permissions = new List<string>();
             RoleRecord roleRecord = GetRole(id);
-            foreach (RolesPermissionsRecord rolesPermission in roleRecord.RolesPermissions) {
+            foreach (RolesPermissionsRecord rolesPermission in roleRecord.RolesPermissions)
+            {
                 permissions.Add(rolesPermission.Permission.Name);
             }
             return permissions;
         }
 
-        public IEnumerable<string> GetPermissionsForRoleByName(string name) {
-            return _cacheManager.Get(name, ctx => {
+        public IEnumerable<string> GetPermissionsForRoleByName(string name)
+        {
+            return _cacheManager.Get(name, ctx =>
+            {
                 MonitorSignal(ctx);
                 return GetPermissionsForRoleByNameInner(name).ToList();
             });
@@ -195,22 +245,26 @@ namespace Orchard.Roles.Services {
         /// </summary>
         /// <param name="name">Role name</param>
         /// <returns>Returns false if a role with the given name already exits</returns>
-        public bool VerifyRoleUnicity(string name) {
+        public bool VerifyRoleUnicity(string name)
+        {
             return (_roleRepository.Get(x => x.Name == name) == null);
         }
-        
 
-        IEnumerable<string> GetPermissionsForRoleByNameInner(string name) {
+
+        IEnumerable<string> GetPermissionsForRoleByNameInner(string name)
+        {
             var roleRecord = GetRoleByName(name);
             return roleRecord == null ? Enumerable.Empty<string>() : GetPermissionsForRole(roleRecord.Id);
         }
 
 
-        private void MonitorSignal(AcquireContext<string> ctx) {
+        private void MonitorSignal(AcquireContext<string> ctx)
+        {
             ctx.Monitor(_signals.When(SignalName));
         }
 
-        private void TriggerSignal() {
+        private void TriggerSignal()
+        {
             _signals.Trigger(SignalName);
         }
     }
