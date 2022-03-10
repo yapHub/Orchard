013cbed682a82ab8cd26187aa5cb4eb5bbe382ad
Michal Szydlowski <michal.szydlowski@futunio.fi>
2022-03-10 08:17:29 +0200

    Revert "security audit changes"
    This reverts commit b4e05fdf30b766de3b1899a0a3cce9ff80eead4c.

--- src/Orchard.Web/Global.asax.cs
+++ src/Orchard.Web/Global.asax.cs
@@ -20,7 +20,6 @@ namespace Orchard.Web {
         }
 
         protected void Application_Start() {
-            MvcHandler.DisableMvcResponseHeader = true;
             RegisterRoutes(RouteTable.Routes);
             _starter = new Starter<IOrchardHost>(HostInitialization, HostBeginRequest, HostEndRequest);
             _starter.OnApplicationStart(this);
--- src/Orchard/Owin/Startup.cs
+++ src/Orchard/Owin/Startup.cs
@@ -8,6 +8,7 @@ namespace Orchard.Owin {
     public class Startup {
         public void Configuration(IAppBuilder app) {
             app.Use((context, next) => {
+                context.Response.Headers.Append("X-Generator", "Orchard");
                 return next();
             });
         }
